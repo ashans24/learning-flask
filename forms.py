00e2824e4609f6f4229ac0f32928ba11882b542a
Ashans Khadka <ashans24@gmail.com>
2018-04-05 19:38:27 -0400

    Created a login, logout and signup system.

--- forms.py
+++ forms.py
@@ -1,9 +1,15 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
 
 class SignupForm(Form):
-	first_name = StringField('First name')
-	last_name = StringField('Last name')
-	email = StringField('Email')
-	password = PasswordField('Password')
+	first_name = StringField('First name', validators=[DataRequired("Please enter your first name")])
+	last_name = StringField('Last name', validators=[DataRequired("Please enter your last name")])
+	email = StringField('Email', validators=[DataRequired("Please enter your email address"), Email("Please enter your email address correctly")])
+	password = PasswordField('Password', validators=[DataRequired("Please enter your password"), Length(min=6, message="Password must be at least 6 characters long")])
 	submit = SubmitField('Sign up')
+
+class LoginForm(Form):
+	email = StringField('Email', validators=[DataRequired("Please enter your email address"), Email("Please enter a correct email address")])
+	password = PasswordField('Password', validators=[DataRequired("Please enter your password")])
+	submit = SubmitField("Sign in")
