20869ac8128834ac6215ab6efbbe4eac45ab2129
Ashans Khadka <ashans24@gmail.com>
2018-04-07 18:42:25 -0400

    Finished the tutorial by using Wiki geodata api and leafletjs and Mapbox to create Maps that display an address and interesting places near that address

--- forms.py
+++ forms.py
@@ -1,15 +1,19 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
 
-class SignupForm(Form):
+class SignupForm(FlaskForm):
 	first_name = StringField('First name', validators=[DataRequired("Please enter your first name")])
 	last_name = StringField('Last name', validators=[DataRequired("Please enter your last name")])
 	email = StringField('Email', validators=[DataRequired("Please enter your email address"), Email("Please enter your email address correctly")])
 	password = PasswordField('Password', validators=[DataRequired("Please enter your password"), Length(min=6, message="Password must be at least 6 characters long")])
 	submit = SubmitField('Sign up')
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
 	email = StringField('Email', validators=[DataRequired("Please enter your email address"), Email("Please enter a correct email address")])
 	password = PasswordField('Password', validators=[DataRequired("Please enter your password")])
 	submit = SubmitField("Sign in")
+
+class AddressForm(FlaskForm):
+	address = StringField('Address', validators=[DataRequired("Please enter an address")])
+	submit = SubmitField("Search")
--- routes.py
+++ routes.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, session, redirect, url_for
-from models import db, User
-from forms import SignupForm, LoginForm
+from models import db, User, Place
+from forms import SignupForm, LoginForm, AddressForm
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI']  = 'postgresql://ashans:Powermate_24@localhost/learningflask'
 db.init_app(app)
 
@@ -19,6 +20,9 @@ def about():
 
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
+	if 'email' in session:
+		return redirect(url_for('home'))
+
 	form = SignupForm()
 
 	if request.method == 'POST':
@@ -35,12 +39,41 @@ def signup():
 	elif request.method == "GET":
 		return render_template('signup.html', form=form)
 
-@app.route("/home")
+@app.route("/home", methods=["GET", "POST"])
 def home():
+	if 'email' not in session:
+		return redirect(url_for('login'))
+
+	form = AddressForm()
+
+	places = []
+	my_coordinates = (37.4224082, -122.0856086)
+
+	if request.method == 'POST':
+		if form.validate() == False:
+			return render_template('home.html', form=form)
+		else:
+			# get the address 
+			address = form.address.data
+
+			# query for places around it
+			p = Place()
+			my_coordinates = p.address_to_latlng(address)
+			places = p.query(address)
+
+			# return those results
+			return render_template('home.html', form=form, my_coordinates=my_coordinates, places=places)
+
+	elif request.method == 'GET':
+		return render_template("home.html", form=form, my_coordinates=my_coordinates, places=places)
+
 	return render_template("home.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+	if 'email' in session:
+		return redirect(url_for('home'))
+
 	form = LoginForm()
 
 	if request.method == "POST":
