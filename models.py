8e595fbafbd5bdbdd5649a470e56b2a79122651c
Ashans Khadka <ashans24@gmail.com>
2018-04-07 20:00:25 -0400

    made changes to use of urlopen and urlparse to accomodate for differences in Python 2 and Python 3

--- models.py
+++ models.py
@@ -2,8 +2,13 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug import generate_password_hash, check_password_hash
 
 import geocoder
-import urllib2
 import json
+
+try:
+	from urllib.request import urlopen
+	from urllib import parse
+except ImportError:
+	from urllib2 import urlopen, urlparse as parse
 
 db = SQLAlchemy()
 
@@ -36,7 +41,7 @@ class Place(object):
 		return int(meters / 80)
 
 	def wiki_path(self, slug):
-		return urllib2.urlparse.urljoin("http://en.wikipedia.org/wiki/", slug.replace(' ', '_'))
+		return parse.urljoin("http://en.wikipedia.org/wiki/", slug.replace(' ', '_'))
 
 	def address_to_latlng(self, address):
 		g = geocoder.google(address)
@@ -46,7 +51,7 @@ class Place(object):
 		lat, lng = self.address_to_latlng(address)
 
 		query_url = 'https://en.wikipedia.org/w/api.php?action=query&list=geosearch&gsradius=5000&gscoord={0}%7C{1}&gslimit=20&format=json'.format(lat, lng)
-		g = urllib2.urlopen(query_url)
+		g = urlopen(query_url)
 		results = g.read()
 		g.close()
 
